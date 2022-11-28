e0238450140dc3041235136be908dbb9d03fa770
jonahlipsky <jonahlipsky@gmail.com>
2022-11-28 16:48:53 -0500

    finish google auth

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import LoginButton from "../components/login-button";
 
 export default function Home() {
   const handleSubmit = async (e) => {
@@ -44,6 +45,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <LoginButton />
 
         <p className={styles.description}>
           Get started by editing{" "}
